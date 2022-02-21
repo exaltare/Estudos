60c28bfe59d74abb4b1189e8cedc782c1f374e29
exaltare <douglas.suporte@outlook.com.br>
2022-02-20 22:23:52 -0300

    Atualizacao

--- Aula20 - Estruturas_While/aula20.cs
+++ Aula20 - Estruturas_While/aula20.cs
@@ -1,1 +1,13 @@
- 
+using System;
+class aula20{
+    static void Main(){
+
+        int i=0;
+        int[] num= new int[10];
+        while (i<10){
+            Console.WriteLine("TESTE DO COMANDO WHILE");
+            i++;
+        } 
+        Console.WriteLine("ESTRUTURA DE FIM DE LOOP");
+    }
+}
