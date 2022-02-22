95008b22a76d7b348071258663b7adeef69ff7d5
exaltare <douglas.suporte@outlook.com.br>
2022-02-22 07:48:53 -0300

    AULA14

---
+++ Aula14 - IF Aninhado/aula14.cs
@@ -0,0 +1,23 @@
+using System;
+class aula14{
+    static void Main(){
+        
+        int nota1, nota2, nota3, resultado;
+
+        Console.WriteLine("POR FAVOR DIGITE A PRIMEIRA NOTA");
+        nota1= int.Parse(Console.ReadLine());
+
+        Console.WriteLine("POR FAVOR DIGITE A SEGUNDA NOTA");
+        nota2= int.Parse(Console.ReadLine());
+
+        Console.WriteLine("POR FAVOR DIGITE A TERCEIRA NOTA");
+        nota3= int.Parse(Console.ReadLine());
+
+
+
+
+
+    }
+
+    
+}
