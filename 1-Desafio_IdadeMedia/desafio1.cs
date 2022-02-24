f93373c2efdb857867efaa003a866f43a30f12f9
exaltare <douglas.suporte@outlook.com.br>
2022-02-24 00:26:27 -0300

    alteracao

---
+++ 1-Desafio_IdadeMedia/desafio1.cs
@@ -0,0 +1,20 @@
+using System;
+    class desafio1{
+        static void Main(){
+
+        
+        string[] aluno= new string[4];
+        float[] nota1= new float[4];
+        float[] nota2= new float[4];
+        float[] nota3= new float[4];
+        float[] nota4= new float[4];
+        float[] media= new float[4];
+
+        for ()
+
+
+
+
+
+        }
+    }
