1bc438f476ef9ac9dc1719ae02300c153e77a8c5
Exaltare <douglas.suporte@outlook.com.br>
2022-02-24 17:42:30 -0300

    AtualizacaoDesafio

--- 1-Desafio_IdadeMedia/desafio1.cs
+++ 1-Desafio_IdadeMedia/desafio1.cs
@@ -2,19 +2,50 @@ using System;
     class desafio1{
         static void Main(){
 
+        int opcao ;
+        //int exercicio;
+
+        Console.WriteLine(">>>>>EXALTARE<<<<<");
+        Console .WriteLine("ESCOLHA QUAL DESAFIO DESEJA ACESSAR : ");
+        Console.WriteLine("[1] DESAFIO IDADE MÉDIA ");
+        Console.WriteLine("[999] SAIR DO PROGRAMA ");
         
-        string[] aluno= new string[4];
-        float[] nota1= new float[4];
-        float[] nota2= new float[4];
-        float[] nota3= new float[4];
-        float[] nota4= new float[4];
-        float[] media= new float[4];
+        opcao = int.Parse(Console.ReadLine());
 
-        for ()
+        switch(opcao){            
+            case 1:  
+             IdadeMedia();
+                break;
 
-
-
-
-
+            case 999:
+                Console.WriteLine("OPCAO INVALIDA");            
+                break;           
         }
+        
+        }
+
+        public static void IdadeMedia(){
+            {
+            
+            int indice;
+            float total =0;
+            float [] idade=new float[4];
+
+            for (indice =0; indice < 4 ; indice++){
+                Console.WriteLine("ENTRE COM A IDADE DO ALUNO" , indice );
+                idade[indice] = float.Parse(Console.ReadLine());
+                
+            }
+
+            Console.WriteLine("IDADES : ");
+            for (indice = 0; indice <4 ; indice++){
+                Console.WriteLine(idade[indice]);
+                total += idade[indice];
+
+            }
+            Console.WriteLine("A IDADE MÉDIA DOS ALUNOS É  :  {0} " , total/4 ); 
+        }
+        }
+
+
     }
