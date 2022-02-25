6c5b20a24c7049a5f53e71920e5a6672066538ae
Exaltare <douglas.suporte@outlook.com.br>
2022-02-25 16:08:04 -0300

    Estudando

---
+++ 2-Exercicio_Maioridade/exercicio2.cs
@@ -0,0 +1,26 @@
+using System;
+class ExercicioMaioridade{
+    static void Main(){
+        int NumeroPessoas;
+        int idade;
+        int i=0;
+        
+
+        Console.WriteLine("QUAL A QUANTIDADE DE PESSOAS QUE DESEJA COMPARAR : ");
+        NumeroPessoas = int.Parse(Console.ReadLine());
+
+        int[] NumeroPessoas = new int[NumeroPessoas];
+        while (i< NumeroPessoas){
+            
+            i = i +1; 
+            Console.WriteLine("QUAL A IDADE DA {0}ª PESSOA " ,i);
+            idade = int.Parse(Console.ReadLine());
+
+        } 
+        Console.WriteLine(" ||||||DADOS COLETADOS|||||| ");
+        Console.WriteLine("UM TOTAL DE {0} : " ,NumeroPessoas);
+        
+        
+
+    }
+}
