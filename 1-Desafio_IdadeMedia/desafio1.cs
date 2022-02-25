5a018e122aaffe5e0c9b42c4a866cd6e5e916468
exaltare <douglas.suporte@outlook.com.br>
2022-02-25 00:52:10 -0300

    DesenvolvendoDesafio

--- 1-Desafio_IdadeMedia/desafio1.cs
+++ 1-Desafio_IdadeMedia/desafio1.cs
@@ -6,24 +6,35 @@ using System;
         //int exercicio;
 
         Console.WriteLine(">>>>>EXALTARE<<<<<");
-        Console .WriteLine("ESCOLHA QUAL DESAFIO DESEJA ACESSAR : ");
+        Console.WriteLine("ESCOLHA QUAL DESAFIO DESEJA ACESSAR : ");
         Console.WriteLine("[1] DESAFIO IDADE MÉDIA ");
+        Console.WriteLine("[2] DESAFIO NUMERO PAR OU IMPAR ");
         Console.WriteLine("[999] SAIR DO PROGRAMA ");
         
         opcao = int.Parse(Console.ReadLine());
 
         switch(opcao){            
             case 1:  
-             IdadeMedia();
+                Console.Clear();
+                IdadeMedia();     //chamado do metodo
                 break;
 
+            case 2:
+                Console.Clear();
+                ParOuImpar();      //chamada do metodo
+                break;
+            
             case 999:
                 Console.WriteLine("OPCAO INVALIDA");            
-                break;           
+                break; 
+
+            default:
+                Console.WriteLine("OPCAO INVALIDA");
+                break;
+                   
         }
         
-        }
-
+    }
         public static void IdadeMedia(){
             {
             
@@ -45,7 +56,38 @@ using System;
             }
             Console.WriteLine("A IDADE MÉDIA DOS ALUNOS É  :  {0} " , total/4 ); 
         }
+        Console.ReadKey();
         }
 
+        public static void ParOuImpar(){
+            int numero; 
 
+            Console.WriteLine("VERIFICAR SE O NUMERO É PAR OU IMPAR");
+            Console.WriteLine("DIGITE O NUMERO PARA FAZER A VERIFICAÇÃO :");
+            numero = int.Parse(Console.ReadLine());
+            
+            if (numero % 2 == 0){
+                Console.WriteLine("O NUMERO {0} É PAR " ,numero );
+            }else
+            {
+                Console.WriteLine("O NUMERO {0} É IMPAR" ,numero);
+
+            }
+            Console.ReadKey();
+            
+            }
+
+        public static void Maiores18Anos(){
+
+            
+
+
+
+
+        }
+
+            
     }
+
+
+
