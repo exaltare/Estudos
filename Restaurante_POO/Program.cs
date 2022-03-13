09c2274d957cfd8662ff8c54fa3101a6fa9aa5d7
exaltare <douglas.suporte@outlook.com.br>
2022-03-13 02:00:23 -0300

    Estudando_POO

---
+++ Restaurante_POO/Program.cs
@@ -0,0 +1,50 @@
+﻿//ETAPA 1->Gravar Nome , Horario , Idade , e Mesa
+//ETAPA 2->Alteracao de valores
+//ETAPA 3->Codicao Idade , incluir 5 mesas
+//ETAPA 4->Sorteio de 3 Pessoas com valor 0
+
+using System;
+using System.Collections.Generic;
+
+namespace Restaurante_POO
+{
+      internal class Program
+    {
+        public static List<ReservaMesa> ListadeMesas = new();
+        static void Main(string[] args)
+        {
+            string Nome;
+            int idade , Numeromesas , mesa =0 ;
+            double Consumo;
+            
+            //PREENCHIMENTO DAS INFORMAÇÕES
+
+            Console.WriteLine("QUANTAS MESAS PARA ESTE EVENTO ");
+            Numeromesas = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < Numeromesas; i++)
+
+            {
+                mesa++;
+                Console.WriteLine("QUAL O NOME DO CLIENTE : ");
+                Nome = Console.ReadLine();
+                Console.WriteLine("QUAL A IDADE DO CLIENTE : ");
+                idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("QUAL O CONSUMO DO CLIENTE :");
+                Consumo = double.Parse(Console.ReadLine());
+
+                Console.Clear();
+                ReservaMesa Reserva1 = new ReservaMesa(mesa, Nome, idade, Consumo);
+                ListadeMesas.Add(Reserva1);
+            } 
+        }
+        static void ListaMesas()
+        {
+            foreach (var item in ListadeMesas)
+            {
+                Console.WriteLine($"{item.NumeroMesa} Ocupada por {item.Nome} ");
+            }
+
+        }
+    }
+}
---
+++ Restaurante_POO/ReservaMesa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante_POO
+{
+    public class ReservaMesa
+    {
+        public int NumeroMesa;
+        public string Nome;
+        public int Idade;
+        public double Consumo;
+
+        public ReservaMesa(int mesa , string Name, int Age, double Consumption)             //CONSTRUTOR SEM VALORES DEFINIDOS
+        {
+            NumeroMesa = mesa;
+            Nome = Name;
+            Idade = Age;
+            Consumo = Consumption;
+
+        }
+
+    }
+}
