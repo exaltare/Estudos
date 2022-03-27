6c0cb2f704b7926f88d0b96345c8a6703bc6383d
exaltare <douglas.suporte@outlook.com.br>
2022-03-27 00:34:22 -0300

    DesafioDioMotos

---
+++ DesafioMotos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+//TODO: Complete os espaços em branco com uma possível solução para o problema.
+class Desafio
+{
+    static void Main()
+    {
+        
+        int quilometros = Int32.Parse(Console.ReadLine());
+        int minutos = quilometros * 2 ; // Digite aqui o calculo dos minutos
+        Console.WriteLine(minutos + " minutos");
+    }
+}
---
+++ EstudandoArray/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EstudandoArray
+{
+    internal class Program
+    {
+
+        static int qtd = 20;
+        static int[] Numeral, Par, Impar;
+        static int IndicePar = 0, IndiceImpar = 0; 
+
+        static void Main(string[] args)
+        {
+            Numeral = new int[qtd];
+            Random r = new Random();
+
+            for (int i = 0; i < Numeral.Length; i++)
+            {
+                Numeral[i] = r.Next(1,100);
+                if (r.Next(1,100)%2==0)
+                {
+                    IndicePar++;
+
+                }
+                else
+                {
+                    IndiceImpar++;
+                }
+            }
+            Par = new int[IndicePar];
+            Impar = new int[IndiceImpar];
+            
+            foreach(var item in Numeral)
+            {
+                if (item%2==0)
+                {
+                    Par[IndicePar - 1] = item;
+                    IndicePar--;
+                }
+            }
+            
+            }
+
+            
+
+
+            
+
+        }
+    }
+}
