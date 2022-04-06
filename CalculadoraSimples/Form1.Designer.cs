ca99b5758690030f28af8a34f1e7a0ed90c2e300
exaltare <douglas.suporte@outlook.com.br>
2022-04-05 23:09:18 -0300

    CalculadoraSimples

---
+++ CalculadoraSimples/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace CalculadoraSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.GrpAritmetica = new System.Windows.Forms.GroupBox();
+            this.rdbDividir = new System.Windows.Forms.RadioButton();
+            this.rdbMultiplicar = new System.Windows.Forms.RadioButton();
+            this.rdbSubtrair = new System.Windows.Forms.RadioButton();
+            this.rdbsomar = new System.Windows.Forms.RadioButton();
+            this.GrpAritmetica.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(35, 16);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(14, 15);
+            this.lblX.TabIndex = 0;
+            this.lblX.Text = "X";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(35, 46);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(14, 15);
+            this.lblY.TabIndex = 1;
+            this.lblY.Text = "Y";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(35, 74);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(32, 15);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "Total";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(126, 13);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 23);
+            this.txtX.TabIndex = 3;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(126, 42);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 23);
+            this.txtY.TabIndex = 4;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(126, 71);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 23);
+            this.txtTotal.TabIndex = 5;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(35, 288);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 6;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(184, 288);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Fechar";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(110, 317);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 8;
+            this.btnFechar.Text = "Calcular";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // GrpAritmetica
+            // 
+            this.GrpAritmetica.Controls.Add(this.rdbDividir);
+            this.GrpAritmetica.Controls.Add(this.rdbMultiplicar);
+            this.GrpAritmetica.Controls.Add(this.rdbSubtrair);
+            this.GrpAritmetica.Controls.Add(this.rdbsomar);
+            this.GrpAritmetica.Location = new System.Drawing.Point(35, 142);
+            this.GrpAritmetica.Name = "GrpAritmetica";
+            this.GrpAritmetica.Size = new System.Drawing.Size(264, 140);
+            this.GrpAritmetica.TabIndex = 10;
+            this.GrpAritmetica.TabStop = false;
+            this.GrpAritmetica.Text = "Operações";
+            // 
+            // rdbDividir
+            // 
+            this.rdbDividir.AutoSize = true;
+            this.rdbDividir.Location = new System.Drawing.Point(10, 107);
+            this.rdbDividir.Name = "rdbDividir";
+            this.rdbDividir.Size = new System.Drawing.Size(59, 19);
+            this.rdbDividir.TabIndex = 3;
+            this.rdbDividir.Text = "Dividir";
+            this.rdbDividir.UseVisualStyleBackColor = true;
+            // 
+            // rdbMultiplicar
+            // 
+            this.rdbMultiplicar.AutoSize = true;
+            this.rdbMultiplicar.Location = new System.Drawing.Point(10, 82);
+            this.rdbMultiplicar.Name = "rdbMultiplicar";
+            this.rdbMultiplicar.Size = new System.Drawing.Size(82, 19);
+            this.rdbMultiplicar.TabIndex = 2;
+            this.rdbMultiplicar.Text = "Multiplicar";
+            this.rdbMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // rdbSubtrair
+            // 
+            this.rdbSubtrair.AutoSize = true;
+            this.rdbSubtrair.Location = new System.Drawing.Point(10, 57);
+            this.rdbSubtrair.Name = "rdbSubtrair";
+            this.rdbSubtrair.Size = new System.Drawing.Size(66, 19);
+            this.rdbSubtrair.TabIndex = 1;
+            this.rdbSubtrair.Text = "Subtrair";
+            this.rdbSubtrair.UseVisualStyleBackColor = true;
+            // 
+            // rdbsomar
+            // 
+            this.rdbsomar.AutoSize = true;
+            this.rdbsomar.Checked = true;
+            this.rdbsomar.Location = new System.Drawing.Point(10, 33);
+            this.rdbsomar.Name = "rdbsomar";
+            this.rdbsomar.Size = new System.Drawing.Size(59, 19);
+            this.rdbsomar.TabIndex = 0;
+            this.rdbsomar.TabStop = true;
+            this.rdbsomar.Text = "Somar";
+            this.rdbsomar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 371);
+            this.Controls.Add(this.GrpAritmetica);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.GrpAritmetica.ResumeLayout(false);
+            this.GrpAritmetica.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.GroupBox GrpAritmetica;
+        private System.Windows.Forms.RadioButton rdbDividir;
+        private System.Windows.Forms.RadioButton rdbMultiplicar;
+        private System.Windows.Forms.RadioButton rdbSubtrair;
+        private System.Windows.Forms.RadioButton rdbsomar;
+    }
+}
---
+++ CalculadoraSimples/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtX.Clear();           //Limpa o texto do Texbox
+            txtTotal.Clear();       //limpa o texto do TextBox
+            txtY.Clear();           //Limpa o texto do TextBox
+
+            rdbsomar.Checked = true;        //deixa selecionado o RadioButton Somar
+            txtX.Focus();                   //coloca o foco do cursr no txtX
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            //declaração das variáveis
+            double x, y, total;
+
+            //entrada de dados
+            //validação de dados
+
+            //verifica se o que foi digitado em txtX é número. Também faz validação se o txtX está vazio.
+            if (double.TryParse(txtX.Text, out x) == false)
+            {
+                //adiciona o botão Ok e o ícone de Informação no MessageBox
+                MessageBox.Show("Informe um valor válido", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtX.Clear();   // limpa o texto do TextBox
+                txtX.Focus();  //coloca o foco do cursor no TextBox
+                return;  //sai da sub-rotina, não executa o código que está abaixo
+            }
+
+            //verifica se o que foi digitado em txty é número. Também faz validação se o txtX está vazio.
+            if (double.TryParse(txtY.Text, out y) == false)
+            {
+                MessageBox.Show("Informe um valor válido", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtY.Clear();   // limpa o texto do TextBox
+                txtY.Focus();  //coloca o foco do cursor no TextBox
+                return;  //sai da sub-rotina, não executa o código que está abaixo
+            }
+
+            //processamento
+            //Testa na estrutura condicional qual RadioButton está selecionado
+
+            if (rdbsomar.Checked == true)
+            {
+                total = x + y;
+            }
+
+            else if (rdbSubtrair.Checked)
+            {
+                total = x - y;
+            }
+            else if (rdbMultiplicar.Checked)
+            {
+                total = x * y;
+            }
+            else
+            {
+                //verifica se está sendo realizada divisão por zero
+                if (y == 0)
+                {
+                    txtTotal.Text = "Divisão por zero";
+                    return;     //sai da sub-rotina, não executa o código que está abaixo 
+                }
+
+                total = x / y;
+            }
+
+            //apresenta a saída de dados convertendo para String
+
+            txtTotal.Text = total.ToString();
+        }
+    }
+}
