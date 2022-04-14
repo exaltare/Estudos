f05b996261a23914d7175fc4526e1d3e426d0615
exaltare <douglas.suporte@outlook.com.br>
2022-04-14 00:48:37 -0300

    Estudando Faculdade

---
+++ MaiorNumero/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MaiorNumero
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblMaior = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtMaior = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(44, 31);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(15, 15);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(44, 65);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(14, 15);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "B";
+            // 
+            // lblMaior
+            // 
+            this.lblMaior.AutoSize = true;
+            this.lblMaior.Location = new System.Drawing.Point(44, 110);
+            this.lblMaior.Name = "lblMaior";
+            this.lblMaior.Size = new System.Drawing.Size(38, 15);
+            this.lblMaior.TabIndex = 2;
+            this.lblMaior.Text = "Maior";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(136, 23);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 23);
+            this.txtA.TabIndex = 3;
+            this.txtA.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(136, 62);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 23);
+            this.txtB.TabIndex = 4;
+            // 
+            // txtMaior
+            // 
+            this.txtMaior.Location = new System.Drawing.Point(136, 107);
+            this.txtMaior.Name = "txtMaior";
+            this.txtMaior.Size = new System.Drawing.Size(100, 23);
+            this.txtMaior.TabIndex = 5;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(255, 24);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 6;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click_1);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(255, 89);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 195);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtMaior);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblMaior);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Verifica Maior";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblMaior;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtMaior;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
---
+++ MaiorNumero/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MaiorNumero
+{
+    public partial class Form1 : Form
+    {
+
+        int verificaMaior(int num1, int num2)
+         
+        
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else  
+            {
+                return num2;
+            }
+            
+
+        }
+
+        void limparControles()
+        {
+            txtA.Clear();
+            txtB.Clear();
+            txtMaior.Clear();
+            txtA.Focus();
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            //declaração das variáveis
+            int a, b;
+
+            //entrada de dados para as variáveis
+            a = Convert.ToInt32(txtA.Text);
+            b = Convert.ToInt32(txtB.Text);
+
+            //chamando a função verificaMaior 
+            //e passando os argumentos para os parâmetros num1 e num2
+            // TextBox txtMaior recebe o retorno da função
+            txtMaior.Text = verificaMaior(a, b).ToString();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //chama o procedimento limparControles
+            limparControles();
+        }
+
+        private void btnVerificar_Click_1(object sender, EventArgs e)
+        {
+            //declaração das variáveis
+            int a, b;
+
+            //entrada de dados para as variáveis
+            a = Convert.ToInt32(txtA.Text);
+            b = Convert.ToInt32(txtB.Text);
+
+            //chamando a função verificaMaior 
+            //e passando os argumentos para os parâmetros num1 e num2
+            // TextBox txtMaior recebe o retorno da função
+            txtMaior.Text = verificaMaior(a, b).ToString();
+        }
+
+        private void btnLimpar_Click_1(object sender, EventArgs e)
+        {
+            //chama o procedimento limparControles
+            limparControles();
+        }
+    }
+}
