694b592b1ac78524e6a18d4a2c03217cf74c4261
exaltare <douglas.suporte@outlook.com.br>
2022-04-18 02:28:21 -0300

    Atualizando Projeto

--- ControleDeContatos/wwwroot/js/site.js
+++ ControleDeContatos/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    getDatatable('#table-contatos');
+    getDatatable('#table-usuarios');
+})
+
+function getDatatable(id) {
+    $(id).DataTable({
+        "ordering": true,                                                                           //Controle de Ordenação
+        "paging": true,                                                                             //Se Vai Ser Paginado
+        "searching": true,                                                                          //Pagina de Busca
+        "oLanguage": {                                                                              //Linguagem
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ at&eacute; _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 at&eacute; 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Proximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+}
+
+
+$('.close-alert').click(function () {
+    $(".alert").hide('hide');
+});
