11a56c1cf3fc612d697b2527477fd5bfbb71e4e1
exaltare <douglas.suporte@outlook.com.br>
2022-05-17 11:46:54 -0300

    Exercicio de Validação de CPF

---
+++ TestarbuscaCEP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DocumentValidator;
+
+namespace TestarbuscaCEP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+                      
+        
+        {
+            string CPF ;
+            Console.WriteLine("POR FAVOR ENTRE COM O NUMERO DO CPF PARA VALIDAÇÃO");
+            CPF = Convert.ToString(Console.ReadLine());
+
+            if (CpfValidation.Validate(CPF))
+                Console.WriteLine("CPF Válido!");
+            else
+                Console.WriteLine("CPF Inválido!");
+
+        }
+
+
+            
+    }
+    
+}
