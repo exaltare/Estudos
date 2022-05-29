6f30499a8af241fac06805667e6cc1553b81b449
exaltare <douglas.suporte@outlook.com.br>
2022-05-28 22:41:56 -0300

    Exercitando  API

---
+++ Produtos_API/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Produtos_API.Models;
+
+namespace Produtos_API.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Carro> Carros { get; set; }
+
+
+
+    }
+}
---
+++ Produtos_API/Models/Carro.cs
@@ -0,0 +1,12 @@
+﻿namespace Produtos_API.Models
+{
+    public class Carro
+    {
+        public int Id { get; set; }
+        public string Placa { get; set; }
+        public string Marca { get; set; }
+        public decimal Preco { get; set; }
+
+
+    }
+}
